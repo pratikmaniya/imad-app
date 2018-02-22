2151b55bb7961adb41d5a2016f70aa9e7ad071c3
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-02-22 23:02:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one.html',function(req,res){
-  res.send(createTemplet(articleOne));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two.html',function(req,res){

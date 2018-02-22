fbcb0257ee6b8b880aba8596381ab67c3c8d81de
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-02-22 23:45:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/', function (req, res) {
 });*/
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', createTemplate(articleOne));
+  res.sendFile(path.join(__dirname, 'ui', createTemplate(articleOne)));
 });
 
 app.get('/ui/style.css', function (req, res) {

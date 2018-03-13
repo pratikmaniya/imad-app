cf90e311d6dd66b374a1a3a9fcd13339c466feaa
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-13 13:23:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/hometree1.css', function (req, res) {
+app.get('/ui/hometree1.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'hometree1.css'));
 });
 

8ffa0e79974d9246ae2a23949b3dbf4ae6b2f4d0
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-14 17:38:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,6 @@ button.onclick = function() {
     request.send(null);
 };
 
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
 var submit = document.getElementById("submitbtn");
 submit.onclick = function() {
     var request = new XMLHttpRequest();
@@ -36,6 +34,8 @@ submit.onclick = function() {
             }
         }
     }
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
     request.open('GET','http://pratikmmaniya244.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     

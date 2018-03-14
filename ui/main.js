e547e9ee1f3a52e2edf90c7e2ff97192d3c41b93
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-14 16:56:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,16 @@ button.onclick = function() {
     request.open('GET','http://pratikmmaniya244.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submitbtn");
+submit.onclick = function() {
+    var names = [name1,name2,name3,name4];
+    var list = '';
+    for(var i=0; i<name.length;i++){
+        list += '<li>'+ names[i] + '<li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};

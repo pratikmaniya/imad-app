03ca8e49bb1b6f240a47abf100a9365dd1df3446
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-14 17:28:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/counter', function(req, res) {
 });
 
 var names = [];
-app.get('submit-name/:name', function (req, res) {
+app.get('/submit-name/:name', function (req, res) {
     var name = req.params.name;
     names.push(name);
     res.send(JSON.stringify(names));

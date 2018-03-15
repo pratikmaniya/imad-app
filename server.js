8bd6dc9f38b0fa33238860d2d8d64d9422f10ce2
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 02:01:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     database: 'pratikmmaniya244',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.db.password
+    password: process.env.DB_PASSWORD
 };
 
 

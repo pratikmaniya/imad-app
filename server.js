811eb033d9bc898877344661ec18f863db3d9917
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 16:02:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,16 +29,6 @@ app.get('/submit-name/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,result){
-       if(err){
-           res.status(500).send(err.toString());
-       } 
-       else{
-           res.send(JSON.stringify(result.rows));
-       }
-    });
-});
 
 app.get('/articles/:articleName', function(req,res) {
     var articleName = req.params.articleName;

87b850fe58be14f7380938e8318705d6a9e4c4f8
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 22:41:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/check-login', function(req, res) {
 
 app.get('/logout', function(req, res) {
     delete req.session.auth;
-    res.send('logged out');
+    alert('logged out');
 });
 
 app.get('/articles/:articleName', function(req,res) {

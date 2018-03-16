6a1ef64569777948e5fd5ac305f52909f8e14b89
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 19:20:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -71,5 +71,36 @@ submit_btn.onclick = function() {
     request.setRequestHeader('content-type','application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
+
+
+var register = document.getElementById("register_btn");
+register.onclick = function() {
+   
+    var request = new XMLHttpRequest();
     
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                console.log('user logged in');
+                alert('loggedin successsfuly');
+            }
+            else if(request.status === 403){
+                alert('username/password is incorrect.');
+            }
+            else if(request.status === 500){
+                alert('something went wrong on server');
+            }
+        }
+    };
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://pratikmmaniya244.imad.hasura-app.io/areate-user',true);
+    request.setRequestHeader('content-type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
+
+
 console.log("loaded");

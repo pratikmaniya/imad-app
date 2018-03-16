4ed4c6f71fa0bfb7e48bc54904c4819d14cfbc33
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 22:17:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,8 +52,6 @@ app.post('/create-user', function(req, res) {
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
-    var qu = 'SELECT * FROM "user" WHERE username = "username"'; 
-    if(qu === NULL){
     pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
        if(err){
             res.status(500).send(err.toString());
@@ -62,10 +60,6 @@ app.post('/create-user', function(req, res) {
             res.send('User successfully created: '+username);
         }     
     });
-    }
-    else{
-        res.send('username already exist');
-    }
 });
 
 app.post('/login', function(req, res) {

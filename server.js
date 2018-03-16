1582c86b8361de0d82093517525b4d48a8e61f46
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 23:36:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -202,23 +202,3 @@ app.get('/get-articles', function (req, res) {
       }
    });
 });
-
-app.get('/get-comments/:articleName', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
-});
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});

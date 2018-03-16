7ae89f794d848dce4cc0493761754c6606254e15
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 21:34:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,139 +42,20 @@ submit.onclick = function() {
     }
 };
 
-var login = document.getElementById("login_btn");
-login.onclick = function() {
-   
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
-                console.log('user logged in');
-                alert('logged in successsfuly');
-            }
-            else if(request.status === 403){
-                alert('username/password is incorrect.');
-            }
-            else if(request.status === 500){
-                alert('something went wrong on server');
-            }
-        }
-    };
-    
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    console.log(username);
-    console.log(password);
-    request.open('POST','http://pratikmmaniya244.imad.hasura-app.io/login',true);
-    request.setRequestHeader('content-type','application/json');
-    request.send(JSON.stringify({username: username, password: password}));
-};
 
 
-var register = document.getElementById("register_btn");
-register.onclick = function() {
-   
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
-                console.log('user logged in');
-                alert('user created successsfuly');
-            }
-            else if(request.status === 403){
-                alert('username/password is incorrect.');
-            }
-            else if(request.status === 500){
-                alert('something went wrong on server');
-            }
-        }
-    };
-    
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    console.log(username);
-    console.log(password);
-    request.open('POST','http://pratikmmaniya244.imad.hasura-app.io/create-user',true);
-    request.setRequestHeader('content-type','application/json');
-    request.send(JSON.stringify({username: username, password: password}));
-};
 
-function loadLoggedInUser (username) {
-    var loginArea = document.getElementById('login_area');
-    loginArea.innerHTML = `
-        <h3> Hi <i>${username}</i></h3>
-        <a href="/logout">Logout</a>
-    `;
-}
 
-function loadLogin () {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                loadLoggedInUser(this.responseText);
-            } else {
-                loadLoginForm();
-            }
-        }
-    };
-    
-    request.open('GET', '/check-login', true);
-    request.send(null);
-}
 
-function loadLoggedInUser (username) {
-    var loginArea = document.getElementById('login_area');
-    loginArea.innerHTML = `
-        <h3> Hi <i>${username}</i></h3>
-        <a href="/logout">Logout</a>
-    `;
-}
 
-function loadLogin () {
-    // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                loadLoggedInUser(this.responseText);
-            } else {
-                loadLoginForm();
-            }
-        }
-    };
-    
-    request.open('GET', '/check-login', true);
-    request.send(null);
-}
 
-function loadArticles () {
-        // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            var articles = document.getElementById('articles');
-            if (request.status === 200) {
-                var content = '<ul>';
-                var articleData = JSON.parse(this.responseText);
-                for (var i=0; i< articleData.length; i++) {
-                    content += `<li>
-                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-                    (${articleData[i].date.split('T')[0]})</li>`;
-                }
-                content += "</ul>"
-                articles.innerHTML = content;
-            } else {
-                articles.innerHTML('Oops! Could not load all articles!')
-            }
-        }
-    };
-    
-    request.open('GET', '/get-articles', true);
-    request.send(null);
-}
+
+
+
+
+
+
+
 
 // The first thing to do is to check if the user is logged in!
 loadLogin();

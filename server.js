add9c5ebce55114acffebb2cf4331349d636a7ed
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 16:04:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/submit-name/', function (req, res) {
     res.send(JSON.stringify(names));
 });
 
-var pool = new Pool(config);
+
 
 app.get('/articles/:articleName', function(req,res) {
     var articleName = req.params.articleName;

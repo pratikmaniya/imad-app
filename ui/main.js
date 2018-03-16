5e2a131ae9e761e289f16fa0529083b56f69852e
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 22:30:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -88,7 +88,7 @@ login.onclick = function() {
             else if(request.status === 500){
                 alert('something went wrong on server');
             }
-            loadLogin();
+           // loadLogin();
         }
     };
     

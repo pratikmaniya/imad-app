995637629f36bc89321e9a061c11daea3ed69acc
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-16 16:24:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/submit-name/', function (req, res) {
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString();
+    return hashed.toString('dec');
 }
 
 app.get('/hash/:input', function(req, res) {

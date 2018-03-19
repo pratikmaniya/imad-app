153469bdee10b1d268704a82feacb3ef9dccee08
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-19 20:11:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,14 +10,7 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 
 
-var Pool = require('pg').Pool;
-var config = {
-    user: 'pratikmmaniya244',
-    database: 'pratikmmaniya244',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-};
+
 
 var pool = new Pool(config);
 

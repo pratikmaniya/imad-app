7fe93cd26d47c54b2e83f813f0508c00b73e2d4e
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-03-28 16:39:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,3 +42,62 @@ submit.onclick = function() {
         request.send(null);
     }
 };
+
+var register = document.getElementById("register_btn");
+register.onclick = function() {
+   
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                console.log('user logged in');
+                alert('user created successsfuly');
+            }
+            else if(request.status === 403){
+                alert('username/password is incorrect.');
+            }
+            else if(request.status === 500){
+                alert('something went wrong on server');
+            }
+        }
+    };
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://pratikmmaniya244.imad.hasura-app.io/create-user',true);
+    request.setRequestHeader('content-type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
+
+var login = document.getElementById("login_btn");
+login.onclick = function() {
+   
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                console.log('user logged in');
+                alert('logged in successsfuly');
+            }
+            else if(request.status === 403){
+                alert('username/password is incorrect.');
+            }
+            else if(request.status === 500){
+                alert('something went wrong on server');
+            }
+            loadLogin();
+        }
+    };
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://pratikmmaniya244.imad.hasura-app.io/login',true);
+    request.setRequestHeader('content-type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};

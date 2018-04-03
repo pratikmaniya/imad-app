ab95bfe1b9ed43376c8e17afa7f26812ce5f3de9
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-04-03 22:32:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -117,11 +117,31 @@ function loadLogin () {
 
 loadLogin();
 
+var nameInput = document.getElementById('name'); 
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-     // Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', '/submit-name?name=' + name, true);
+var request = new XMLHttpRequest();
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+      // Take some action
+          if (request.status === 200) {
+              // Capture a list of names and render it as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0; i< names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;   
+          }
+      }  
+      // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://coco98.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);  
- };
+ }; 

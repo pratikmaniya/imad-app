15a714ea671d6e0ba76a5fe1d40e7fe2017dd862
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-04-03 22:39:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -145,3 +145,14 @@ var request = new XMLHttpRequest();
     request.open('GET', '/submit-name?name=' + name, true);
     request.send(null);  
  }; 
+ 
+ var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function () {
+  // Make a request to the counter endpoint
+  // Capture the response if the request was succesfully completed
+  // Render the response in the HTML
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+ };

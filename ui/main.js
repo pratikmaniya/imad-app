14632085c80b5d4e32269005a6ddf2fd692077a0
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-04-03 22:49:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -161,6 +161,6 @@ button.onclick = function () {
             }
         }  
     };
-    request.open('GET', 'http://coco98.imad.hasura-app.io/counter', true);
+    request.open('GET', '/counter', true);
     request.send(null);
 };

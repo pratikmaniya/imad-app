7d10bd35a8ba7c20e4b515cd7cd941cc5cae12dc
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-04-15 00:09:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.post('/login', function(req, res) {
 
 app.get('/check-login', function (req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+       pool.query('SELECT * FROM "newuser" WHERE id = $1', [req.session.auth.userId], function (err, result) {
            if (err) {
               res.status(500).send(err.toString());
            } else {

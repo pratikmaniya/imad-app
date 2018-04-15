1f28c6109f733ed9ba4385905a38b26c22889a64
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-04-15 15:45:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,37 +24,6 @@ var config = {
 
 var pool = new Pool(config);
 
-app.post('/submit-comment/:articleName', function (req, res) {
-   // Check if the user is logged in
-    if (req.session && req.session.auth && req.session.auth.userId) {
-        // First check if the article exists and get the article-id
-        pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
-            if (err) {
-                res.status(500).send(err.toString());
-            } else {
-                if (result.rows.length === 0) {
-                    res.status(400).send('Article not found');
-                } else {
-                    var articleId = result.rows[0].id;
-                    // Now insert the right comment for this article
-                    pool.query(
-                        "INSERT INTO comment (article_id, user_id, comment) VALUES ($1, $2, $3)",
-                        [articleId, req.session.auth.userId, req.body.comment],
-                        function (err, result) {
-                            if (err) {
-                                res.status(500).send(err.toString());
-                            } else {
-                                res.status(200).send('Comment inserted!');
-                            }
-                        });
-                }
-            }
-       });     
-    } else {
-        res.status(403).send('Only logged in users can comment');
-    }
-});
-
 var counter = 0;
 app.get('/counter', function(req, res) {
     counter = counter + 1;
@@ -273,20 +242,6 @@ app.get('/articles/:articleName', function (req, res) {
         }
     }
   });
-});
-
-
-
-app.get('/get-comments/:articleName', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
 });
 
 var port = 80;

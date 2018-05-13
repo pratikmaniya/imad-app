6858aa17ced396ddbf7eadf0a26d99284261d944
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-05-13 11:17:41 +0530

    [imad-console] Updates ui/other.js

--- ui/other.js
+++ ui/other.js
@@ -11,7 +11,7 @@ function loadRegisterForm () {
 		  </center>
 		  <p>Username</p>
 		  <center>
-		  <input type="text" name="hidden" class="infield"  id ="nusername" autocomplete="false">
+		  <input type="text" name="hidden" class="infield"  id ="nusername" autocomplete="off">
 		  </center>
 		  <p>Password</p>
 		  <center>
